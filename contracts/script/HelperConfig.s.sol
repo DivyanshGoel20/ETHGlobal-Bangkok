e85daa6d1af33397a0afa233e54456b1170c7444
megabyte0x <yashygkf@gmail.com>
2024-11-17 00:42:52 +0700

    feat: flow deployment ✅

--- contracts/script/HelperConfig.s.sol
+++ contracts/script/HelperConfig.s.sol
@@ -6,7 +6,7 @@ import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 
 contract HelperConfig is Script {
     function getBlackJack(uint256 _chainId) public view returns (address) {
-        address blackJack = DevOpsTools.get_most_recent_deployment("BlackJack", block.chainid);
+        address blackJack = DevOpsTools.get_most_recent_deployment("BlackJack", _chainId);
         return blackJack;
     }
 
