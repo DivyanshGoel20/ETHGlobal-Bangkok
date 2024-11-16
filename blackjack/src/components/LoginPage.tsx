99d77263d0875b07ca37835368f75148d930107c
0xPrakharG <prakhargoyal.3014@gmail.com>
2024-11-16 18:03:47 +0700

    fix: error

--- blackjack/src/components/LoginPage.tsx
+++ blackjack/src/components/LoginPage.tsx
@@ -5,7 +5,7 @@
 //     const connectWalletHandler = () => {
 //         setLoading(true)
 //         try {
-            
+
 //         } catch (error) {
 //             console.error(error)
 //             setLoading(false)
@@ -50,7 +50,7 @@ export default function LoginPage() {
             setTimeout(() => {
                 navigate("/user"); // Navigate to /user page
             }, 3000); // Optional delay to show full-page loader
-        } catch (error) {
+        } catch (error: any) {
             console.error(error);
             alert("An error occurred: " + error.message); // Show error alert
             setLoading(false); // Stop button loader on error
@@ -59,27 +59,24 @@ export default function LoginPage() {
 
     return (
         <div className="overflow-hidden max-h-screen h-screen bg-[url('./bg-home.svg')] text-center flex flex-col items-center gap-96">
-
-                    <div className="w-[90%] pb-20 mt-20">
-                        <h1 className="text-4xl md:text-5xl font-bold mb-4 text-[#FBD07D] raleway">
-                            Play BlackJack!
-                        </h1>
-                        <p className="text-[#FBD07D] text-[10px] max-w-md raleway">
-                            Lorem ipsum dolor sit amet consectetur. Faucibus ullamcorper
-                            facilisis Lorem ipsum dolor sit amet consectetur.
-                        </p>
-                    </div>
-                    <div className="-mt-20">
-                        <button
-                            className="bg-gradient-to-b helvetica shadow-[0px_2px_2px_0px] shadow-[#A2AFA889] from-[#E7BD70] to-[#F3D495] text-base font-medium rounded-[10px] py-[10px] px-10 hover:bg-gradient-to-t"
-                            onClick={connectWalletHandler}
-                            disabled={loading}
-                        >
-                            {loading ? <Loading loading /> : "Connect Wallet"}
-                        </button>
-                    </div>
-
-
+            <div className="w-[90%] pb-20 mt-20">
+                <h1 className="text-4xl md:text-5xl font-bold mb-4 text-[#FBD07D] raleway">
+                    Play BlackJack!
+                </h1>
+                <p className="text-[#FBD07D] text-[10px] max-w-md raleway">
+                    Lorem ipsum dolor sit amet consectetur. Faucibus ullamcorper
+                    facilisis Lorem ipsum dolor sit amet consectetur.
+                </p>
+            </div>
+            <div className="-mt-20">
+                <button
+                    className="bg-gradient-to-b helvetica shadow-[0px_2px_2px_0px] shadow-[#A2AFA889] from-[#E7BD70] to-[#F3D495] text-base font-medium rounded-[10px] py-[10px] px-10 hover:bg-gradient-to-t"
+                    onClick={connectWalletHandler}
+                    disabled={loading}
+                >
+                    {loading ? <Loading loading /> : "Connect Wallet"}
+                </button>
+            </div>
         </div>
     );
 }
