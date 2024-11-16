ad90ec469d5db4ec9e83ea58fa7672af4713c690
0xPrakharG <prakhargoyal.3014@gmail.com>
2024-11-16 17:01:11 +0700

    error

--- blackjack/src/components/Table.tsx
+++ blackjack/src/components/Table.tsx
@@ -20,7 +20,7 @@ interface PlayerProps {
 export default function Table() {
     const [step, setStep] = useState<string>("1");
     // @ts-ignore
-    const [win, setWin] = useState<boolean>(true);
+    const [win, setWin] = useState<boolean>(false);
 
     // CARD STORES
     const { cards: dealerCards, newCard: newDealerCard } = useDealerCardStore();
